841fa828ef814f6938f2b1af13ba3b9693b68ea9
konghayao <dongzhongzhidong@qq.com>
2022-05-03 19:34:27 +0800

    feat: 添加颜色标识

--- src/components/Dependence.tsx
+++ src/components/Dependence.tsx
@@ -1,115 +1,9 @@
-import {
-    createSignal,
-    lazy,
-    Suspense,
-    Show,
-    onMount,
-    createEffect,
-    onCleanup,
-} from "solid-js";
+import { createSignal } from "solid-js";
 import "../style/dependence.css";
-import g6, { Graph, NodeConfig, EdgeConfig, GraphData } from "@antv/g6";
-const G6: typeof g6 = (globalThis as any).G6;
-G6.registerNode("remote-module", {}, "circle");
+import type { NodeConfig, EdgeConfig } from "@antv/g6";
+import { RenderMap } from "./RenderMap";
+import { isURLString } from "../utils/isURLString";
 
-const RenderMap = (props: { data(): GraphData }) => {
-    let container!: HTMLDivElement;
-    let graph: Graph;
-    onMount(() => {
-        graph = new G6.Graph({
-            container,
-            width: 400,
-            height: 300,
-            layout: {
-                type: "force",
-                preventOverlap: true, // 防止节点重叠
-                linkDistance: 50,
-                nodeSize: 70,
-            },
-
-            defaultNode: {
-                type: "rect",
-                size: 16, // 节点大小
-                // 节点样式配置
-                style: {
-                    fill: "steelblue", // 节点填充色
-                    stroke: "#666", // 节点描边色
-                    lineWidth: 1, // 节点描边粗细
-                    opacity: 0.2,
-                },
-
-                // 节点上的标签文本配置
-                labelCfg: {
-                    // 节点上的标签文本样式配置
-                    size: 8,
-                    style: {
-                        fill: "#222", // 节点标签文字颜色
-                    },
-                },
-            },
-            defaultEdge: {
-                // 边样式配置
-                style: {
-                    opacity: 0.6, // 边透明度
-                    stroke: "#aa0000", // 边描边颜色
-                    endArrow: {
-                        path: G6.Arrow.triangleRect(7, 7, 7, 3, 5, 15),
-                        d: 5,
-                        fill: "#aa0000",
-                    },
-                },
-            },
-
-            modes: {
-                default: [
-                    "drag-node",
-                    "drag-canvas",
-                    "zoom-canvas",
-                    {
-                        type: "tooltip", // 提示框
-                        formatText(model) {
-                            // 提示框文本内容
-                            const text = "url: " + model.name;
-                            return text;
-                        },
-                    },
-                ],
-            },
-            nodeStateStyles: {
-                // 鼠标 hover 上节点，即 hover 状态为 true 时的样式
-                hover: {
-                    stroke: "#222", // 节点描边色
-                },
-            },
-        });
-
-        graph.data(props.data());
-        graph.render();
-        createEffect(() => {
-            graph.clear();
-            console.log("数据更新", props.data());
-
-            graph.data(props.data());
-            graph.render();
-        });
-
-        if (typeof window !== "undefined")
-            window.onresize = () => {
-                if (!graph || graph.get("destroyed")) return;
-                if (
-                    !container ||
-                    !container.scrollWidth ||
-                    !container.scrollHeight
-                )
-                    return;
-                graph.changeSize(container.scrollWidth, container.scrollHeight);
-            };
-    });
-    onCleanup(() => {
-        graph.destroy();
-    });
-    return <div class="relative" ref={container}></div>;
-};
 type NodeMeta = {
     id: string;
     imported: { uid: string }[];
@@ -121,6 +15,7 @@ export default function Dependence() {
         edges: [] as EdgeConfig[],
     });
     (globalThis as any).RollupHub.on("drawDependence", ({ nodeMetas }: any) => {
+        // console.log(nodeMetas);
         const edges: EdgeConfig[] = [];
         const nodes = Object.entries<NodeMeta>(nodeMetas).map(
             ([uid, value]) => {
@@ -132,8 +27,21 @@ export default function Dependence() {
                         };
                     })
                 );
+                let type = "circle";
+                let fill = "blue";
+                if (value.id.startsWith(window.location.origin)) {
+                    type = "local";
+                    fill = "blue";
+                } else if (/https?:\//.test(value.id)) {
+                    type = "remote";
+                    fill = "green";
+                } else {
+                    console.log(value.id);
+                }
                 return {
                     id: uid,
+                    type,
+                    style: { fill },
                     name: value.id,
                     label: value.id.replace(/.*\//, ""),
                 };
@@ -145,7 +53,7 @@ export default function Dependence() {
         });
     });
     return (
-        <section class="flex flex-col bg-gray-50/20 backdrop-blur text-gray-700 p-2 overflow-y-auto h-full">
+        <section class="flex flex-col bg-gray-50/20 backdrop-blur text-gray-700 p-2 overflow-y-auto h-full  justify-center">
             <RenderMap data={dependence}></RenderMap>
         </section>
     );
