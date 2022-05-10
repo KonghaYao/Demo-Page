57ec3ab4ad484f665d725dc0d079493d503fe1d1
konghayao <dongzhongzhidong@qq.com>
2022-05-10 10:34:46 +0800

    feat: 添加 tips 显示

--- src/HelperBar.tsx
+++ src/HelperBar.tsx
@@ -3,28 +3,45 @@ import { updateStore } from "./components/dependencePanel/ModuleStore";
 import { PageSearch } from "./components/pageSearch";
 import { System } from "./components/System";
 import { CDN } from "./global";
-
+import "xy-ui/components/xy-tips.js";
+import { For } from "solid-js";
 export function HelperBar() {
-
+    const icons = [
+        {
+            value: "fitbit",
+            click: () => updateStore("dependence", "show", true),
+            class: ["bg-green-400"],
+            tips: "打包依赖图",
+        },
+        {
+            value: "source",
+            click: () => {
+                const url = new URL(`/src/pages/${System.moduleName}.tsx`, CDN);
+                CodeViewerEvent.emit("showCode", url.toString());
+            },
+            class: ["bg-orange-400"],
+            tips: "主要源代码",
+        },
+    ];
     return (
         <nav class="flex p-2 px-4 bg-gray-100 items-center">
             <div class="grid grid-flow-col gap-4 text-white  items-center">
-                <div
-                    class="material-icons rounded-full p-1 bg-green-400"
-                    onclick={() => updateStore("dependence", "show", true)}>
-                    fitbit
-                </div>
-                <div
-                    class="material-icons bg-orange-400 rounded-full p-1"
-                    onclick={() => {
-                        const url = new URL(
-                            `/src/pages/${System.moduleName}.tsx`,
-                            CDN
+                <For each={icons}>
+                    {(item) => {
+                        return (
+                            // @ts-ignore
+                            <xy-tips dir="bottom" attr:tips={item.tips}>
+                                <div
+                                    className={item.class.join(" ")}
+                                    class="material-icons cursor-pointer rounded-full p-1 "
+                                    onclick={item.click}>
+                                    {item.value}
+                                </div>
+                                {/* @ts-ignore */}
+                            </xy-tips>
                         );
-                        CodeViewerEvent.emit("showCode", url.toString());
-                    }}>
-                    source
-                </div>
+                    }}
+                </For>
             </div>
             <PageSearch></PageSearch>
         </nav>
