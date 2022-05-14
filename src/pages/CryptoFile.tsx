84e801602c1d1c8bc57ad80d4849b94c1f7a7d01
konghayao <dongzhongzhidong@qq.com>
2022-05-14 15:56:31 +0800

    feat: 添加图片展示

--- src/pages/CryptoFile.tsx
+++ src/pages/CryptoFile.tsx
@@ -1,5 +1,5 @@
 import type Sodium from "libsodium-wrappers";
-import { Component, createSignal, For, lazy, Suspense } from "solid-js";
+import { Component, For, lazy, Suspense } from "solid-js";
 import { ModuleDescription } from "../components/ModuleDescription";
 
 // 在 npm 上没有 browser 版本
@@ -21,9 +21,9 @@ export const description: ModuleDescription = {
 
 /** 获取全局的属性 */
 const sodium = useGlobal<typeof Sodium>("sodium");
-interface ImageShower {
-    url: string;
-    desc: string;
+export interface ImageShower {
+    src: string;
+    description: string;
     title: string;
     size: number;
     time: number;
@@ -33,13 +33,16 @@ import { Loading } from "../components/LoadingPage/loading";
 /** 渲染每一个图片 */
 const RenderImage: Component<{
     data: ImageShower;
+    onclick: () => void;
 }> = (props) => {
     return (
         <div className="col-span-1 flex flex-col items-center h-full">
             <div className="font-bold">{props.data.title}</div>
-            <div className="text-sm text-gray-500">{props.data.desc}</div>
+            <div className="text-sm text-gray-500">
+                {props.data.description}
+            </div>
             <div className="flex-grow my-2">
-                <img src={props.data.url}></img>
+                <img src={props.data.src} onclick={() => props.onclick()}></img>
             </div>
             <div className="text-sm text-gray-500">
                 {/* @ts-ignore */}
@@ -50,14 +53,15 @@ const RenderImage: Component<{
     );
 };
 
+import { showImage } from "../utils/showImage";
 const AsyncCrypto = lazy(async () => {
     const images: ImageShower[] = [];
     const pushImage = (
         result: ArrayBuffer,
-        options: Omit<ImageShower, "url" | "size">
+        options: Omit<ImageShower, "src" | "size">
     ) => {
         const url = URL.createObjectURL(new Blob([result]));
-        images.push({ url, ...options, size: result.byteLength });
+        images.push({ src: url, ...options, size: result.byteLength });
     };
     // 主要加解密功能实现
     await sodium.ready
@@ -69,8 +73,8 @@ const AsyncCrypto = lazy(async () => {
             const result = new Uint8Array(chunk);
             const time = timeCounter("getFile")!;
             pushImage(chunk, {
-                title: "原始数据",
-                desc: "输入的二进制数据",
+                title: "导入原始数据",
+                description: "输入的二进制数据",
                 time,
             });
             return result;
@@ -80,8 +84,8 @@ const AsyncCrypto = lazy(async () => {
             const result = Encryption("123456", new Uint8Array(chunk));
             const time = timeCounter("encrypt")!;
             pushImage(result, {
-                title: "加密后的结果",
-                desc: "文件头+文件（分隔符）文件（结束符号）",
+                title: "加密过程与结果",
+                description: "文件头+文件（分隔符）文件（结束符号）",
                 time,
             });
             return result;
@@ -91,16 +95,21 @@ const AsyncCrypto = lazy(async () => {
             const result = Decryption("123456", encryptedChunk);
             const time = timeCounter("decrypt")!;
             pushImage(result!, {
-                title: "解密后的文件",
-                desc: "解密文件",
+                title: "解密文件过程",
+                description: "解密文件",
                 time,
             });
         });
+
     return {
         default: () => {
             return (
                 <For each={images}>
-                    {(data) => <RenderImage data={data}></RenderImage>}
+                    {(data, index) => (
+                        <RenderImage
+                            onclick={() => showImage(images, index())}
+                            data={data}></RenderImage>
+                    )}
                 </For>
             );
         },
@@ -108,10 +117,10 @@ const AsyncCrypto = lazy(async () => {
 });
 export default function CryptoFile() {
     return (
-        <div className="grid  items-center space-y-3 grid-cols-3 my-4">
-            <Suspense fallback={<Loading></Loading>}>
+        <Suspense fallback={<Loading></Loading>}>
+            <div className="grid gap-2 items-center space-y-3 grid-cols-3 my-4">
                 <AsyncCrypto></AsyncCrypto>
-            </Suspense>
-        </div>
+            </div>
+        </Suspense>
     );
 }
