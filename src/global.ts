d7e155e68274a9a78eb9115206003a53c89c604a
konghayao <dongzhongzhidong@qq.com>
2022-05-14 20:52:11 +0800

    license: 添加证书

--- src/global.ts
+++ src/global.ts
@@ -1,2 +1,3 @@
-export const CDN =
-    window.location.href || 'https"//cdn.jsdelivr.net/gh/konghayao/';
+/** 配置 CDN 选项，如果使用 CDN 的话 */
+export const CDN = window.location.href;
+// export const CDN =  'https"//cdn.jsdelivr.net/gh/konghayao/';
