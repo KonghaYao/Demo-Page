763d91f9fc408ff35f2e47e18477e2111e975008
konghayao <dongzhongzhidong@qq.com>
2022-05-14 10:37:13 +0800

    style: 修改样式

--- src/PageList/index.tsx
+++ src/PageList/index.tsx
@@ -1,14 +1,14 @@
 import { For } from "solid-js";
 import { ModuleDescription } from "../components/ModuleDescription";
 import { CDN } from "../global";
-import { Link } from "../router";
+import { Link } from "../router/index";
 
 const pageList: ModuleDescription[] = await fetch(
     new URL("./script/PageList.json", CDN)
 ).then((res) => res.json());
 export const PageList = () => {
     return (
-        <div>
+        <div class="grid grid-cols-4 gap-2">
             <For each={pageList}>
                 {(data) => <PageCard data={data}></PageCard>}
             </For>
@@ -17,12 +17,14 @@ export const PageList = () => {
 };
 const PageCard = (props: { data: ModuleDescription }) => {
     return (
-        <div>
-            <div>{props.data.title}</div>
-            <div>{props.data.desc}</div>
-            <Link
-                href={`/page/${props.data.fileName}.tsx`}
-                element={<button>跳转</button>}></Link>
+        <div class="flex flex-col p-4 rounded-xl bg-white/80 backdrop-blur-md">
+            <header>{props.data.title}</header>
+            <div class="flex-grow">{props.data.desc}</div>
+            <footer>
+                <Link
+                    href={`/page/${props.data.fileName}`}
+                    element={<button>跳转</button>}></Link>
+            </footer>
         </div>
     );
 };
