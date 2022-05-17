93c103f82167d55f4f674d61a492b79c86369ec8
konghayao <dongzhongzhidong@qq.com>
2022-05-18 07:37:00 +0800

    feat: 加速 xmind

--- src/pages/xmind.tsx
+++ src/pages/xmind.tsx
@@ -1,11 +1,18 @@
 import { createEffect, createResource, createSignal, onMount } from "solid-js";
 import { ModuleDescription } from "../components/ModuleDescription";
-import { XMindEmbedViewer } from "xmind-embed-viewer";
+
+// 修改一下 插件里面 的 iframe 地址，让其指向我的加速网页
+/** xmind-embed-viewer */
+import { XMindEmbedViewer } from "../utils/xmind/index";
+
 export const description: ModuleDescription = {
     fileName: "xmind",
     title: "XMind 思维导图支持",
     desc: "在网页端打开思维导图",
-    link: ["https://github.com/xmindltd/xmind-embed-viewer"],
+    link: [
+        "https://www.xmind.cn/",
+        "https://github.com/xmindltd/xmind-embed-viewer",
+    ],
 };
 
 export default function () {
@@ -20,17 +27,27 @@ export default function () {
         new XMindEmbedViewer({
             el,
             file: file(),
+            url: new URL(
+                "./assets/xmind.html",
+                window.location.href
+            ).toString(),
         });
     });
     return (
         <div class="flex flex-col justify-center items-center h-full w-full">
-            <input
-                type="file"
-                oninput={async (e) => {
-                    const file = await e.currentTarget.files![0].arrayBuffer();
-                    mutate(file);
-                }}></input>
-            <div>xmind 官网在线加载较慢，请稍等。。。</div>
+            <div class="flex ">
+                <input
+                    type="file"
+                    oninput={async (e) => {
+                        const file =
+                            await e.currentTarget.files![0].arrayBuffer();
+                        mutate(file);
+                    }}></input>
+                <div>
+                    xmind
+                    官网在线加载较慢，所以在我的项目里面加速了一下官方的网页
+                </div>
+            </div>
             <div class="rounded-lg overflow-hidden" ref={el!}></div>
         </div>
     );
