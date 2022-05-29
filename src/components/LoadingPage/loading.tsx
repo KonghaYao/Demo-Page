76225eac171c380aa20fc1f43d34786ea65b9bee
konghayao <3446798488@qq.com>
2022-05-29 09:49:16 +0800

    feat: 添加等候时间

--- src/components/LoadingPage/loading.tsx
+++ src/components/LoadingPage/loading.tsx
@@ -1,4 +1,4 @@
-import { Component, mergeProps } from "solid-js";
+import { Component, createSignal, mergeProps, onCleanup } from "solid-js";
 import { getAssets } from "../../utils/getAssets";
 const SpinnerNames: string[] = await getAssets(
     "./assets/spinnerName.json",
@@ -18,10 +18,17 @@ export const Loading: Component<{
         props
     );
     const el = document.createElement(props.spinner!);
+    const [counter, setCounter] = createSignal(0);
+    const tag = setTimeout(() => setCounter(counter() + 1), 1000);
+    onCleanup(() => {
+        clearTimeout(tag);
+    });
     return (
         <div className="h-full w-full flex-col flex justify-center items-center">
             {el}
-            <span className="p-4">{props.message}</span>
+            <span className="p-4">
+                已等候 {counter} 秒 - {props.message}
+            </span>
         </div>
     );
 };
