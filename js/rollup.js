045b327f83649cd52490be25b1da128c6fe33acc
konghayao <3446798488@qq.com>
2022-06-14 16:32:50 +0800

    feat: 完成修复工作

--- js/rollup.js
+++ js/rollup.js
@@ -68,12 +68,12 @@ const compiler = new Compiler(RollupConfig, {
     log(url) {
         console.log("%c Download ==> " + url, "color:green");
     },
-    // useDataCache: {
-    //     ignore: isDev
-    //         ? ["src/pages/*.tsx", "script/PageList.json"].map((i) => CDN + i)
-    //         : [],
-    //     maxAge: 24 * 60 * 60,
-    // },
+    useDataCache: {
+        ignore: isDev
+            ? ["src/pages/*.tsx", "script/PageList.json"].map((i) => CDN + i)
+            : [],
+        maxAge: 24 * 60 * 60,
+    },
     extraBundle: [],
 });
 
